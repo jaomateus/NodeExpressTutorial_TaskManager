437d0e852176cdaac16ec8394f454a1f6e2ed0ef
Joao Mateus <jaomateus@gmail.com>
2023-09-22 20:17:56 +0100

    finish controllers

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,20 +5,11 @@ const tasks = require("./routes/tasks");
 require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 //route
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
-
-// app.get('/api/v1/tasks')  - get all the tasks
-// app.post('/api/v1/tasks')  -  create a new tasks
-// app.get('api/v1/tasks/:id')  -  get a single tasks
-// app.patch('/api/v1/tasks/:id')  -  update tasks
-// app.delete('/api/v1/tasks/:id')  -  delete task
 
 const port = 3000;
 const start = async () => {
