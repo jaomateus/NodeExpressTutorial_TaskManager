fbd515c3fdea16031035585332b5e83b1b374122
Joao Mateus <jaomateus@gmail.com>
2023-09-22 14:48:45 +0100

    create get all tasks controller

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,8 @@
+const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -19,5 +21,13 @@ app.use("/api/v1/tasks", tasks);
 // app.delete('/api/v1/tasks/:id')  -  delete task
 
 const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+start();
