045f56fed3bada50d4ce447211b89bb92e635ecf
Joao Mateus <jaomateus@gmail.com>
2023-09-22 14:52:12 +0100

    create get task controller

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -18,8 +18,13 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.find({ id: req.params.id });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
