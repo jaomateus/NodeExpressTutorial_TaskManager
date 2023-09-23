7c254dd74c4c1f9b5ca64c322d7c8c59e5086a4c
Joao Mateus <jaomateus@gmail.com>
2023-09-23 01:34:38 +0100

    refactor controllers

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,15 +3,20 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
-//route
+//routes
 app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
---
+++ 03-task-manager/starter/middleware/async.js
@@ -0,0 +1,11 @@
+const asynWrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = asynWrapper;
